0d473bb7e06901cbb8dae8e355979d87873818e8
hidayatullah191001 <hidayatullahd745@gmail.com>
2021-09-28 16:08:43 +0700

    Coba CatalogMovie

---
+++ app/src/main/java/com/example/catalogmovie3/DetailMovieActivity.java
@@ -0,0 +1,16 @@
+package com.example.catalogmovie3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.catalogmovie3.R;
+
+public class DetailMovieActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_movie);
+    }
+}
